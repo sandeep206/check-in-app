c357576348e20791f10bef87fd85164608f6a636
t440960 <sandeep.sasidharan@klm.com>
2016-10-13 15:24:28 +0200

    first commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+ 
+
 export class AppComponent {
-  title = 'app works!';
+
+  registerForm = new FormGroup({
+    bookingcode: new FormControl(),
+    familyname: new FormControl()
+ 
+  });
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
+import { NgModule } from '@angular/core'; 
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http'; 
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,8 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule, 
+    ReactiveFormsModule,
+    MaterialModule.forRoot(),
     HttpModule
   ],
   providers: [],
